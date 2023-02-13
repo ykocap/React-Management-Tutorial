1693d7968589f92ee0a45e71f7857a0f4f2f5bcf
ykocap <ykocap1@gmail.com>
2023-02-13 22:27:35 +0900

    Customer Add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: '홍길동',
+    birthday: '830102',
+    gender: '남자',
+    job: '대학생',
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '개똥이',
+    birthday: '830102',
+    gender: '여자',
+    job: '경리',
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: '성유리',
+    birthday: '830102',
+    gender: '남자',
+    job: '디자이너',
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {customer.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
       </div>
     );
   }
